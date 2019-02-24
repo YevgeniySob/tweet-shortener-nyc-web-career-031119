232e9f02fa377cf5511a5d4f361c62d9a117571c
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-24 04:27:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,23 +3,15 @@ def dictionary
   dictionary = {
     "hello" => "hi"
     "to" => "2",
-    "too" => "2",
-    
     "two" =>"2",
-    "four" => "4",
+    "too" => "2",
     "for" => "4",
+    "four" => "4",
     "be" => "b",
     "you" => "u",
     "at" => "@",
     "and" => "&"
   }
-"hello" becomes 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
 end
 
 def work_substituter(str)
