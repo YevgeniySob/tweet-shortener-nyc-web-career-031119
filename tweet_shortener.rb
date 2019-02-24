715e6d49b9256c82b2f366828264dc2c9e3958b0
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-24 07:07:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,7 @@ def bulk_tweet_shortener(arr)
 end
 
 def selective_tweet_shortener(tweet)
-  tweet.length > 140 ? tweet[0...140] : tweet
+  tweet.length > 140 ? word_substituter(tweet) : tweet
 end
 
 def shortened_tweet_truncator(tweet)
