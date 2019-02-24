1445fdba4c1e5d8d30aae599f77ef34f792cd8ef
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-24 04:26:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,17 @@
 # Write your code here.
 def dictionary
   dictionary = {
-  "too" => "2",
-  "to" => "2",
-  "two" =>"2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
+    "hello" => "hi"
+    "to" => "2",
+    "too" => "2",
+    
+    "two" =>"2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 "hello" becomes 'hi'
 "to, two, too" become '2' 
