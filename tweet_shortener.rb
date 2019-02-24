de3f1e4444c6af6958066c2f0b1efa96a55b384b
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-24 04:10:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,14 @@
 # Write your code here.
 def dictionary
+"hello" becomes 'hi'
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"
+end
+
+def work_substituter(str)
   
 end
